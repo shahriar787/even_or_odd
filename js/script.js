54ae968dba844d496b52a8ce36529a22c62d29ed
Shahriar Shakil <30352466+shahriar787@users.noreply.github.com>
2017-10-04 05:08:50 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var number = Number(prompt("Enter first Number"));
+
+if (number % 2 == 0) {
+    document.write(number + " Is a even number");
+} else {
+    document.write(number + " Is a odd number");
+}
